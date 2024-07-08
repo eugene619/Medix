277285ab6685273ce1bec6651a38a8aa70ce410c
eugene619 <eugeneamatika619@gmail.com>
2024-07-08 10:58:58 +0300

    initial commit

---
+++ medixapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from medixapp.models import Product, Branch, Contact
+
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(Branch)
+admin.site.register(Contact)
+
